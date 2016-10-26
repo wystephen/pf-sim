4104d3de5324e1e3a197ae3b785239d35044c232
wang stephen <551619855@qq.com>
2016-10-26 20:46:27 +0800

    beacon

--- Beacon.py
+++ Beacon.py
@@ -18,5 +18,42 @@ class BeaconWithRange:
         :param ScaleFactor: Real(m) to piexels
         '''
 
+        self.SCREEN_SIZE = SCREEN_SIZE
+        self.OFFSET = OFFSET
+        self.SCALEFACTOR = ScaleFactor
+
+        self.Pose = [10,10]
+        self.Dist  = 0
+
+        self.IntPose = [1,1]
+        self.IntDist = 0
+
+    def SetPose(self,x,y):
+        self.Pose = [x,y]
+        for i in range(len(self.Pose)):
+            self.IntPose[i] = int(self.Pose[i]*1.0/self.SCALEFACTOR) + self.OFFSET[i]
+
+
+    def SetRange(self,distance):
+        self.Dist = distance
+        self.IntDist = int(distance * 1.0 / self.SCALEFACTOR)
+
+    def Draw(self,screen):
+        pygame.draw.circle(screen,
+                           [0,22,211],
+                           self.IntPose,
+                           self.IntDist,1)
+
+    def ComputeRange(self,the_pose):
+        tmp_distance = 0.0
+        for i in range(len(self.Pose)):
+            tmp_distance += (self.Pose[i]*1.0 - the_pose[i]*1.0) ** 2.0
+        tmp_distance = tmp_distance ** 0.5
+
+        self.SetRange(tmp_distance)
+
+
+
+
 
 
--- draw_test.py
+++ draw_test.py
@@ -4,50 +4,75 @@
 
 import pygame
 
-# --- Clobals ---
-# Colors
+from Beacon import BeaconWithRange
 
-BLACK=(0,0,0)
-WHITE=(255,255,255)
+if __name__ == '__main__':
+    # --- Clobals ---
+    # Colors
 
-SCREEN_SIZE=[1680,980]
+    BLACK=(0,0,0)
+    WHITE=(255,255,255)
 
-OFFSET = [10,10] # piexels
+    SCREEN_SIZE=[1680,980]
 
-ScaleFactor = 1.0 #Real(m) to piexels
+    OFFSET = [10,10] # piexels
 
-
-pygame.init()
-
-screen = pygame.display.set_mode(SCREEN_SIZE)
-
-pygame.display.set_caption("Test All")
-
-allspriteslit = pygame.sprite.Group()
-
-clock = pygame.time.Clock()
-done = False
-
-while not done:
-    pose = pygame.mouse.get_pos()
-
-    for event  in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-        elif event.type == pygame.KEYDOWN:
-            print(event.key)
+    ScaleFactor = 1.0 #Real(m) to piexels
 
 
+    pygame.init()
 
-    screen.fill(BLACK)
-    pygame.draw.circle(screen,[0,10,155],pose,20,3)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+
+    pygame.display.set_caption("Test All")
+
+    allspriteslit = pygame.sprite.Group()
+
+    clock = pygame.time.Clock()
+    done = False
+
+    tmp_beacon = BeaconWithRange(SCREEN_SIZE,OFFSET,ScaleFactor)
+    tmp_beacon2 = BeaconWithRange(SCREEN_SIZE,OFFSET,ScaleFactor)
+    tmp_beacon3 = BeaconWithRange(SCREEN_SIZE,OFFSET,ScaleFactor)
+
+    tmp_beacon.SetPose(100,100)
+    tmp_beacon2.SetPose(1000,500)
+
+    tmp_beacon3.SetPose(1300,200)
 
 
-    pygame.display.flip()
 
-    # clock.tick(5)
+    while not done:
+        pose = pygame.mouse.get_pos()
 
-pygame.quit()
+        for event  in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            elif event.type == pygame.KEYDOWN:
+                print(event.key)
+
+
+        screen.fill(BLACK)
+        pygame.draw.circle(screen,[110,10,155],pose,20,3)
+
+
+        tmp_beacon.ComputeRange(pose)
+        tmp_beacon2.ComputeRange(pose)
+
+        tmp_beacon3.ComputeRange(pose)
+
+        # tmp_beacon.SetRange(pose[1]+2)
+        tmp_beacon.Draw(screen)
+        tmp_beacon2.Draw(screen)
+        tmp_beacon3.Draw(screen)
+
+
+
+        pygame.display.flip()
+
+        # clock.tick(5)
+
+    pygame.quit()
 
 
